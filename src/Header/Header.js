0fd94831b2c78119dd43924f1337543f346214ea
Sami Melhem <SamiISaboss23@gmail.com>
2023-01-28 16:17:56 -0600

    no borders

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,18 +4,13 @@ function Header() {
     return (
     <div className="header">
           <h1>TFA</h1>
-          <div className='homeButton'>
+          <div className='rightButtons'>
             <button>
                 <h1>Home</h1>
             </button>
-            
-          </div>
-          <div className='forumButton'>
             <button>
                 <h1>Forum</h1>
             </button>
-          </div>
-          <div className='menu'>
             <button>
                 <h1>---</h1>
             </button>
