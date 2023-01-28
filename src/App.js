4837bd6a98aff23eaf2e1c191915b23aae7841b1
Chandler Ngo <chandlerngo00@gmail.com>
2023-01-28 16:39:24 -0600

    added articles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header.js';
+import Articles from './Articles/Articles.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Articles/>
     </div>
   );
 }
