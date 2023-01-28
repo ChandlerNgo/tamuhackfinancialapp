2be16cd1b6e9d34ebf0e1af62e2c7afd2b4c8915
Sami Melhem <SamiISaboss23@gmail.com>
2023-01-28 17:19:59 -0600

    Clean design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header/Header.js';
 import Articles from './Articles/Articles.js';
+import Line from './Line/line.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Line/>
       <Articles/>
     </div>
   );
